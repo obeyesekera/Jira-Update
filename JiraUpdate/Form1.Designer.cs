dae77e4fb3081e9b8b69a54b3a8f28d509c339b7
Nuwan Obeyesekera <nuwan.obeyesekera@scicom.com.my>
2024-07-05 19:05:26 +0530

    Add project files.

---
+++ JiraUpdate/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace JiraUpdate
+{
+    partial class mainFrm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainFrm));
+            this.txtBugIDs = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtBugIDs
+            // 
+            this.txtBugIDs.Location = new System.Drawing.Point(13, 123);
+            this.txtBugIDs.Multiline = true;
+            this.txtBugIDs.Name = "txtBugIDs";
+            this.txtBugIDs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBugIDs.Size = new System.Drawing.Size(495, 279);
+            this.txtBugIDs.TabIndex = 0;
+            this.txtBugIDs.TextChanged += new System.EventHandler(this.txtBugIDs_TextChanged);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(513, 322);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(112, 34);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(12, 49);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(614, 31);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(13, 86);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(613, 31);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(514, 368);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(112, 34);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(13, 12);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(613, 31);
+            this.txtURL.TabIndex = 5;
+            // 
+            // mainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 417);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtBugIDs);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainFrm";
+            this.Text = "JiraUpdate";
+            this.Load += new System.EventHandler(this.mainFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBugIDs;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtURL;
+    }
+}
+
---
+++ JiraUpdate/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace JiraUpdate
+{
+    public partial class mainFrm : Form
+    {
+        private int updateCount = 0;
+        private int bugCount = 0;
+        private string[] textBoxLines;
+        private string jUser;
+        private string jKey;
+        private string jURL;
+        private List<KeyValuePair<string, string>> tranIDs;
+
+        public mainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            btnUpdate.Enabled = false;
+            btnClear.Enabled = false;
+            txtBugIDs.Enabled = false;
+
+            textBoxLines = txtBugIDs.Lines;
+
+            for (int i=0; i<textBoxLines.Length; i++)
+            {
+                string bugID = ReplaceWhitespace(textBoxLines[i].Trim(), ""); 
+
+                if (bugID.Length > 0)
+                {
+                    bugCount++;
+
+                    string tranID = getTranID(bugID);
+
+                    if (tranID!="ERROR") { 
+                        DoUpdate(bugID, tranID, i);
+                    }
+                    else
+                    {
+                        
+                        textBoxLines[i] = bugID + "  - ERROR";
+                        txtBugIDs.Lines = textBoxLines;
+                        bugCount++;
+                    }
+                }
+            }
+
+            btnClear.Enabled = true;
+            txtBugIDs.Enabled = true;
+        }
+
+        private string getTranID(string bgID)
+        {
+            string prjCode = "";
+            try { 
+            string[] prjCodeArr = bgID.Split("-");
+                prjCode = prjCodeArr[0];
+
+            string value = (from kvp in tranIDs where kvp.Key == prjCodeArr[0] select kvp.Value).Last();
+
+            return value.ToString();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Module '"+ prjCode + "' not found");
+                return "ERROR";
+            }
+        }
+
+
+
+        private async void DoUpdate(string bugID, string transitionID, int rowID)
+        {
+            
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("POST"), jURL + "/rest/api/2/issue/"+ bugID + "/transitions?expand=transitions.fields"))
+                {
+                    var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes(jUser + ":" + jKey));
+                    request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
+
+                    request.Content = new StringContent("{\"transition\":{\"id\":\""+ transitionID + "\"}}");
+                    request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                    var response = await httpClient.SendAsync(request);
+
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        updateCount++;
+                        textBoxLines[rowID] = bugID + "  - Updated";
+                        txtBugIDs.Lines = textBoxLines;
+                    }
+                    else
+                    {
+                        textBoxLines[rowID] = bugID + "  - ERROR";
+                        txtBugIDs.Lines = textBoxLines;
+                    }
+
+
+                }
+            }
+        }
+
+        private void mainFrm_Load(object sender, EventArgs e)
+        {
+
+            txtURL.Enabled = false;
+            jURL = ConfigurationManager.AppSettings["jiraURL"];
+            txtURL.Text = jURL;
+
+            txtUsername.Enabled = false;
+            jUser = ConfigurationManager.AppSettings["jiraUser"]; 
+            txtUsername.Text = jUser;
+
+            txtPassword.Enabled = false;
+            txtPassword.PasswordChar = '*';
+            jKey = ConfigurationManager.AppSettings["jApiKey"]; 
+            txtPassword.Text = jKey;
+
+
+            var tranList = new List<KeyValuePair<string, string>>();
+
+            foreach (string key in ConfigurationManager.AppSettings)
+            {
+                if (!key.StartsWith("j"))
+                {
+                    string value = ConfigurationManager.AppSettings[key];
+
+                    tranList.Add(new KeyValuePair<string, string>(key, value));
+
+                }
+
+            }
+
+            tranIDs = tranList;
+
+
+            txtPassword.Text = jKey;
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            btnUpdate.Enabled = true;
+            txtBugIDs.Enabled = true;
+            txtBugIDs.Clear();
+        }
+
+        private void txtBugIDs_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private static readonly Regex sWhitespace = new Regex(@"\s+");
+        public static string ReplaceWhitespace(string input, string replacement)
+        {
+            return sWhitespace.Replace(input, replacement);
+        }
+    }
+}
